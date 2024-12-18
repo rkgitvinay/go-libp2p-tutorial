22aa0976bd291d5ed4e0e60ab9792184f647160c
Vinay Singh <vinayksingh2@gmail.com>
2024-12-18 17:57:47 +0530

    file discovery

--- main.go
+++ main.go
@@ -4,285 +4,246 @@ import (
 	"bufio"
 	"context"
 	"crypto/rand"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"log"
-	mrand "math/rand"
+	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/crypto"
+	libHost "github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/peerstore"
-
 	"github.com/multiformats/go-multiaddr"
 )
 
+type FileMetadata struct {
+	Name string `json:"name"`
+	Size int64  `json:"size"`
+}
+
 var (
-	streams    sync.Map                     // Existing map for peer streams
-	knownPeers = make(map[peer.ID]struct{}) // Tracks known peer IDs
-	peersMutex sync.Mutex
+	host       libHost.Host
+	peerFiles  = make(map[string][]FileMetadata) // Map of peer ID to their files
+	localFiles []FileMetadata                    // Files on this node
+	filesMutex sync.Mutex                        // Mutex to protect file lists
+	filesDir   = "./shared"                      // Directory to store uploaded files
 )
 
 func handleStream(s network.Stream) {
-	peerID := s.Conn().RemotePeer()
-	log.Printf("New stream from: %s\n", peerID)
+	log.Printf("Established stream with peer: %s\n", s.Conn().RemotePeer().String())
 
-	// Add peer to known peers
-	peersMutex.Lock()
-	knownPeers[peerID] = struct{}{}
-	peersMutex.Unlock()
-
-	// Create a buffered read-writer for the stream
 	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
 
-	// Store the stream for bidirectional communication
-	streams.Store(peerID, rw)
+	go func() {
+		// Continuously read data from the stream
+		for {
+			str, err := rw.ReadString('\n')
+			if err != nil {
+				log.Printf("Error reading from stream: %v", err)
+				return
+			}
+			if str != "" {
+				var peerFilesReceived []FileMetadata
+				err := json.Unmarshal([]byte(str), &peerFilesReceived)
+				if err != nil {
+					log.Printf("Error unmarshaling peer files: %v", err)
+					continue
+				}
 
-	// Handle reading and writing in separate goroutines
-	go readData(rw, peerID)
-	go writeData()
+				// Update the peerFiles map with received data
+				peerID := s.Conn().RemotePeer().String()
+				filesMutex.Lock()
+				peerFiles[peerID] = peerFilesReceived
+				filesMutex.Unlock()
 
-	// Keep the stream open
+				log.Printf("Updated files from peer %s: %v", peerID, peerFilesReceived)
+
+				// Broadcast the received metadata to all other connected peers
+				broadcastToPeers(peerID, peerFilesReceived)
+			}
+		}
+	}()
 }
 
-func readData(rw *bufio.ReadWriter, peerID peer.ID) {
+func broadcastToPeers(senderID string, files []FileMetadata) {
+	filesJSON, _ := json.Marshal(files)
+
+	for _, conn := range host.Network().Conns() {
+		peerID := conn.RemotePeer().String()
+		if peerID == senderID {
+			continue // Skip broadcasting back to the sender
+		}
+
+		stream, err := host.NewStream(context.Background(), conn.RemotePeer(), "/fileshare/1.0.0")
+		if err != nil {
+			log.Printf("Failed to open stream to peer %s: %v", peerID, err)
+			continue
+		}
+
+		rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+		_, err = rw.WriteString(fmt.Sprintf("%s\n", filesJSON))
+		if err != nil {
+			log.Printf("Failed to send file metadata to peer %s: %v", peerID, err)
+		}
+		_ = rw.Flush()
+	}
+}
+
+func broadcastFileMetadata() {
+	filesMutex.Lock()
+	localFilesJSON, _ := json.Marshal(localFiles)
+	filesMutex.Unlock()
+
+	for _, conn := range host.Network().Conns() {
+		stream, err := host.NewStream(context.Background(), conn.RemotePeer(), "/fileshare/1.0.0")
+		if err != nil {
+			log.Printf("Failed to open stream to peer %s: %v", conn.RemotePeer().String(), err)
+			continue
+		}
+
+		rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+		_, err = rw.WriteString(fmt.Sprintf("%s\n", localFilesJSON))
+		if err != nil {
+			log.Printf("Failed to send file metadata to peer %s: %v", conn.RemotePeer().String(), err)
+		}
+		_ = rw.Flush()
+	}
+}
+
+func connectToPeer(peerAddr string) error {
+	maddr, err := multiaddr.NewMultiaddr(peerAddr)
+	if err != nil {
+		return fmt.Errorf("invalid multiaddress: %v", err)
+	}
+
+	info, err := peer.AddrInfoFromP2pAddr(maddr)
+	if err != nil {
+		return fmt.Errorf("failed to parse peer address: %v", err)
+	}
+
+	host.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
+
+	// Open a stream to the peer
+	s, err := host.NewStream(context.Background(), info.ID, "/fileshare/1.0.0")
+	if err != nil {
+		return fmt.Errorf("failed to create stream: %v", err)
+	}
+
+	// Handle the stream
+	handleStream(s)
+	return nil
+}
+
+func readData(rw *bufio.ReadWriter) {
 	for {
 		str, _ := rw.ReadString('\n')
-
 		if str == "" {
-			log.Printf("Connection with peer %s closed.\n", peerID)
-			streams.Delete(peerID)
-			peersMutex.Lock()
-			delete(knownPeers, peerID) // Remove peer from knownPeers
-			peersMutex.Unlock()
 			return
 		}
-
 		if str != "\n" {
-			switch {
-			case str == "LIST\n":
-				// Respond with all known peer IDs
-				peersMutex.Lock()
-				for p := range knownPeers {
-					rw.WriteString(fmt.Sprintf("PEER:%s\n", p))
-				}
-				rw.Flush()
-				peersMutex.Unlock()
-			case len(str) > 5 && str[:5] == "PEER:":
-				// Register received peer ID
-				newPeer := str[5 : len(str)-1]
-				peersMutex.Lock()
-				knownPeers[peer.ID(newPeer)] = struct{}{}
-				peersMutex.Unlock()
-				fmt.Printf("Discovered Peer: %s\n", newPeer)
-			case len(str) > 5 && str[:5] == "FILE:":
-				handleFileReceive(str[5:], rw)
-			default:
-				// Display chat message
-				fmt.Printf("[PEER %s] %s> ", peerID, str)
-
-				// Forward the message to all other connected peers
-				streams.Range(func(key, value interface{}) bool {
-					if key != peerID { // Exclude the original sender
-						peerRW := value.(*bufio.ReadWriter)
-						peerRW.WriteString(fmt.Sprintf("[FORWARDED from %s]: %s", peerID, str))
-						peerRW.Flush()
-					}
-					return true
-				})
-			}
+			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
 		}
 	}
 }
 
-func handleFileReceive(metadata string, rw *bufio.ReadWriter) {
-	// Extract filename and size from metadata
-	var filename string
-	var filesize int64
-	fmt.Sscanf(metadata, "%s %d", &filename, &filesize)
-
-	newFileName := "./received/" + filepath.Base(filename)
-	fmt.Printf("Receiving file: %s (%d bytes)\n", filename, filesize)
-
-	file, err := os.Create(newFileName)
-	if err != nil {
-		fmt.Printf("Failed to create file: %v\n", err)
-		return
-	}
-	defer file.Close()
-
-	received := int64(0)
-	buf := make([]byte, 4096)
-
-	for received < filesize {
-		n, err := rw.Read(buf)
-		if err != nil {
-			fmt.Printf("Error reading file: %v\n", err)
-			return
-		}
-
-		_, err = file.Write(buf[:n])
-		if err != nil {
-			fmt.Printf("Error writing file: %v\n", err)
-			return
-		}
-
-		received += int64(n)
-	}
-
-	fmt.Printf("File %s received and saved successfully.\n", newFileName)
-
-	// Forward the file to all other connected peers
-	forwardFile(newFileName, filesize, rw)
-}
-
-func forwardFile(filename string, filesize int64, senderRW *bufio.ReadWriter) {
-	fmt.Printf("Forwarding file: %s (%d bytes)\n", filename, filesize)
-
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("Failed to open file for forwarding: %v\n", err)
-		return
-	}
-	defer file.Close()
-
-	// Send file metadata and data to all connected peers except the sender
-	streams.Range(func(key, value interface{}) bool {
-		peerRW := value.(*bufio.ReadWriter)
-		if peerRW != senderRW { // Avoid sending back to the sender
-			// Send metadata
-			peerRW.WriteString(fmt.Sprintf("FILE:%s %d\n", filepath.Base(filename), filesize))
-			peerRW.Flush()
-
-			// Send file data
-			buf := make([]byte, 4096)
-			for {
-				n, err := file.Read(buf)
-				if err != nil {
-					if err == io.EOF {
-						break
-					}
-					fmt.Printf("Error reading file: %v\n", err)
-					return false
-				}
-				peerRW.Write(buf[:n])
-				peerRW.Flush()
-			}
-		}
-		return true
-	})
-
-	fmt.Printf("File %s forwarded successfully.\n", filename)
-}
-
-func writeData() {
+func writeData(rw *bufio.ReadWriter) {
 	stdReader := bufio.NewReader(os.Stdin)
-
 	for {
 		fmt.Print("> ")
 		sendData, err := stdReader.ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
-
-		sendData = sendData[:len(sendData)-1] // Trim newline character
-
-		switch {
-		case sendData == "list":
-			// Send LIST command to all peers
-			fmt.Println("Requesting list of connected peers...")
-			streams.Range(func(key, value interface{}) bool {
-				peerRW := value.(*bufio.ReadWriter)
-				peerRW.WriteString("LIST\n")
-				peerRW.Flush()
-				return true
-			})
-		case len(sendData) > 5 && sendData[:5] == "send:":
-			// Broadcast file to all connected peers
-			filename := sendData[5:]
-			broadcastFile(filename)
-		default:
-			// Broadcast chat message to all peers
-			streams.Range(func(key, value interface{}) bool {
-				peerRW := value.(*bufio.ReadWriter)
-				peerRW.WriteString(fmt.Sprintf("%s\n", sendData))
-				peerRW.Flush()
-				return true
-			})
-		}
+		rw.WriteString(fmt.Sprintf("%s\n", sendData))
+		rw.Flush()
 	}
 }
 
-func broadcastFile(filename string) {
-	file, err := os.Open(filename)
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST is supported", http.StatusMethodNotAllowed)
+		return
+	}
+
+	file, header, err := r.FormFile("file")
 	if err != nil {
-		fmt.Printf("Failed to open file: %v\n", err)
+		http.Error(w, "Failed to read file", http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
-	info, err := file.Stat()
+	// Save file locally
+	filePath := filepath.Join(filesDir, header.Filename)
+	os.MkdirAll(filesDir, os.ModePerm)
+	out, err := os.Create(filePath)
 	if err != nil {
-		fmt.Printf("Failed to get file info: %v\n", err)
+		http.Error(w, "Failed to save file", http.StatusInternalServerError)
 		return
 	}
-	filesize := info.Size()
+	defer out.Close()
 
-	// Send file metadata and data to all connected peers
-	streams.Range(func(key, value interface{}) bool {
-		rw := value.(*bufio.ReadWriter)
+	size, err := io.Copy(out, file)
+	if err != nil {
+		http.Error(w, "Failed to save file", http.StatusInternalServerError)
+		return
+	}
 
-		// Send metadata
-		rw.WriteString(fmt.Sprintf("FILE:%s %d\n", filename, filesize))
-		rw.Flush()
+	// Add to local files
+	filesMutex.Lock()
+	localFiles = append(localFiles, FileMetadata{Name: header.Filename, Size: size})
+	filesMutex.Unlock()
 
-		// Send file data
-		buf := make([]byte, 4096)
-		for {
-			n, err := file.Read(buf)
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				fmt.Printf("Error reading file: %v\n", err)
-				return false
-			}
-			rw.Write(buf[:n])
-			rw.Flush()
-		}
-		return true
+	// Broadcast the updated metadata to all peers
+	broadcastFileMetadata()
+
+	fmt.Fprintf(w, "File uploaded successfully: %s", header.Filename)
+}
+
+func getFiles(w http.ResponseWriter, r *http.Request) {
+	filesMutex.Lock()
+	defer filesMutex.Unlock()
+	json.NewEncoder(w).Encode(localFiles)
+}
+
+func getPeersAndFiles(w http.ResponseWriter, r *http.Request) {
+	peerData := map[string][]FileMetadata{}
+	filesMutex.Lock()
+	for peerID, files := range peerFiles {
+		peerData[peerID] = files
+	}
+	filesMutex.Unlock()
+
+	json.NewEncoder(w).Encode(peerData)
+}
+
+func startWebInterface(port int) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
 	})
+	http.HandleFunc("/upload", uploadFile)
+	http.HandleFunc("/files", getFiles)
+	http.HandleFunc("/peers", getPeersAndFiles)
 
-	fmt.Printf("File %s broadcasted successfully.\n", filename)
+	addr := fmt.Sprintf(":%d", port)
+	log.Printf("Starting web interface at http://localhost%s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func main() {
 	sourcePort := flag.Int("sp", 0, "Source port number")
 	dest := flag.String("d", "", "Destination multiaddr string")
-	help := flag.Bool("help", false, "Display help")
-	debug := flag.Bool("debug", false, "Debug generates the same node ID on every execution")
-
+	port := flag.Int("port", 8080, "Port for the web interface")
 	flag.Parse()
 
-	if *help {
-		fmt.Printf("This program demonstrates a multi-node p2p chat and file-sharing application using libp2p\n\n")
-		fmt.Println("Usage:")
-		fmt.Println("  Listener: ./chat -sp <SOURCE_PORT>")
-		fmt.Println("  Dialer: ./chat -d <MULTIADDR>")
-		os.Exit(0)
-	}
-
 	var r io.Reader
-	if *debug {
-		r = mrand.New(mrand.NewSource(int64(*sourcePort)))
-	} else {
-		r = rand.Reader
-	}
+	r = rand.Reader
 
 	prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
 	if err != nil {
@@ -290,64 +251,32 @@ func main() {
 	}
 
 	sourceMultiAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", *sourcePort))
-
-	host, err := libp2p.New(
-		// context.Background(),
+	host, err = libp2p.New(
 		libp2p.ListenAddrs(sourceMultiAddr),
 		libp2p.Identity(prvKey),
 	)
-
 	if err != nil {
 		panic(err)
 	}
 
-	host.SetStreamHandler("/chat/1.0.0", handleStream)
-
-	if *dest == "" {
-		// Listener mode
-		var port string
-		for _, la := range host.Network().ListenAddresses() {
-			if p, err := la.ValueForProtocol(multiaddr.P_TCP); err == nil {
-				port = p
-				break
-			}
-		}
-
-		if port == "" {
-			panic("Was not able to find actual local port")
-		}
-
-		fmt.Printf("Run './chat -d /ip4/127.0.0.1/tcp/%v/p2p/%s' on another console.\n", port, host.ID())
-		fmt.Printf("OR\n")
-		fmt.Printf("Run 'go run main.go -d /ip4/127.0.0.1/tcp/%v/p2p/%s' on another console.\n", port, host.ID())
-		fmt.Println("Waiting for incoming connections...")
-
-		select {}
-	} else {
-		// Dialer mode
-		maddr, err := multiaddr.NewMultiaddr(*dest)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		info, err := peer.AddrInfoFromP2pAddr(maddr)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		host.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
-
-		s, err := host.NewStream(context.Background(), info.ID, "/chat/1.0.0")
-		if err != nil {
-			panic(err)
-		}
-
-		rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
-		streams.Store(info.ID, rw)
-
-		go writeData()
-		go readData(rw, info.ID)
-
-		select {}
+	fmt.Printf("Node started with ID: %s\n", host.ID())
+	fmt.Println("Listening on addresses:")
+	for _, addr := range host.Addrs() {
+		fmt.Printf(" - %s/p2p/%s\n", addr.String(), host.ID())
 	}
+
+	go startWebInterface(*port)
+
+	// Set stream handler for the file-sharing protocol
+	host.SetStreamHandler("/fileshare/1.0.0", handleStream)
+
+	// Connect to destination peer if provided
+	if *dest != "" {
+		err := connectToPeer(*dest)
+		if err != nil {
+			log.Fatalf("Failed to connect to peer: %v", err)
+		}
+	}
+
+	select {}
 }
